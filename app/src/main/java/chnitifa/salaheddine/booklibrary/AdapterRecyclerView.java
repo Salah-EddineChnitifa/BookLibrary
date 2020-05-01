92f1c8dbe49fde45f2fdf9770f5db7dc9cdb044f
Lenovo-ThinkPad <salah.chnitfa@gmail.com>
2020-05-01 20:13:11 +0100

    Create AdapterRecyclerView

--- app/src/main/java/chnitifa/salaheddine/booklibrary/AdapterRecyclerView.java
+++ app/src/main/java/chnitifa/salaheddine/booklibrary/AdapterRecyclerView.java
@@ -1,4 +1,49 @@
 package chnitifa.salaheddine.booklibrary;
 
-public class AdapterRecyclerView {
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class AdapterRecyclerView extends RecyclerView.Adapter<AdapterRecyclerView.ViewHolder> {
+
+    private Context context;
+    private Activity activity;
+    private ArrayList book_id, book_title, book_author;
+
+    AdapterRecyclerView(Activity activity, Context context, ArrayList book_id, ArrayList book_title, ArrayList book_author,
+        ArrayList book_pages) {
+        this.activity = activity;
+        this.context = context;
+        this.book_id = book_id;
+        this.book_title = book_title;
+        this.book_author = book_author;
+    }
+
+    @NonNull
+    @Override
+    public AdapterRecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterRecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
