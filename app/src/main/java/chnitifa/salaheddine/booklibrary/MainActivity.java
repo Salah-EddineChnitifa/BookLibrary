b245c6bb9750b21e68591a43122f81856554350f
Lenovo-ThinkPad <salah.chnitfa@gmail.com>
2020-05-01 18:16:06 +0100

    Create custom column recyclerview

--- app/src/main/java/chnitifa/salaheddine/booklibrary/MainActivity.java
+++ app/src/main/java/chnitifa/salaheddine/booklibrary/MainActivity.java
@@ -3,8 +3,11 @@ package chnitifa.salaheddine.booklibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 import chnitifa.salaheddine.booklibrary.databinding.ActivityMainBinding;
 
@@ -31,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
         View layoutView=getLayoutInflater().inflate(R.layout.add_book,null);
         builder.setView(layoutView);
         AlertDialog dialog=builder.create();
+        dialog.setCancelable(false);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         dialog.show();
     }
 }
