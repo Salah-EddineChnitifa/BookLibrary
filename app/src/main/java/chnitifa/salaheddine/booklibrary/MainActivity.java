b63e6996d6c4a78c63cef6a706b4d9a14cd064fd
Lenovo-ThinkPad <salah.chnitfa@gmail.com>
2020-05-01 19:03:22 +0100

    Create dialogAddbook in MainActivity

--- app/src/main/java/chnitifa/salaheddine/booklibrary/MainActivity.java
+++ app/src/main/java/chnitifa/salaheddine/booklibrary/MainActivity.java
@@ -8,6 +8,8 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 
 import chnitifa.salaheddine.booklibrary.databinding.ActivityMainBinding;
 
@@ -33,11 +35,32 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog.Builder builder= new AlertDialog.Builder(this);
         View layoutView=getLayoutInflater().inflate(R.layout.add_book,null);
         builder.setView(layoutView);
-        AlertDialog dialog=builder.create();
+        final AlertDialog dialog=builder.create();
         dialog.setCancelable(false);
         dialog.setCanceledOnTouchOutside(false);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        Button add=findViewById(R.id.buttonAddBook);
+        Button cancel=findViewById(R.id.button1Cancel);
+        final EditText title_input = findViewById(R.id.editTitreBook);
+        final EditText author_input = findViewById(R.id.editAuthorBook);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseHelper DB = new DatabaseHelper(MainActivity.this);
+                DB.addBook(title_input.getText().toString().trim(),
+                        author_input.getText().toString().trim()
+                        );
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
         dialog.show();
     }
 }
