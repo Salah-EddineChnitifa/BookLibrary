cced16940d0defd635c60fab765c8d25dd5178d4
Lenovo-ThinkPad <salah.chnitfa@gmail.com>
2020-05-03 14:42:17 +0100

    Add SplashActivity

--- app/src/main/java/chnitifa/salaheddine/booklibrary/SplashActivity.java
+++ app/src/main/java/chnitifa/salaheddine/booklibrary/SplashActivity.java
@@ -3,12 +3,23 @@ package chnitifa.salaheddine.booklibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import chnitifa.salaheddine.booklibrary.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
-
+    ActivitySplashBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.splash);
+        binding.logo.setAnimation(anim);
+        binding.bookapp.setAnimation(anim);
+
+
     }
 }
